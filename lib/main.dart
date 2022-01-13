a93d30863dcb60bc599c4bd6541e95ccab4ec334
ffw-dev <93714234+ffw-dev@users.noreply.github.com>
2022-01-14 00:55:22 +0100

    DevEzaApi class created and exposed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 
+import 'dart:html';
+
 import 'package:dev_eza_api/endpoints/authentication_endpoints.dart';
 import 'package:dev_eza_api/endpoints/ezAsset_endpoints.dart';
+import 'package:dev_eza_api/endpoints/ezFile_endpoints.dart';
+import 'package:dev_eza_api/endpoints/ezLabel_endpoints.dart';
+import 'package:dev_eza_api/endpoints/ezProject_endpoints.dart';
 import 'package:dev_eza_api/endpoints/session_endpoints.dart';
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'main.config.dart';
+
+export 'response_models/ezAsset.dart';
+export 'response_models/ezDestination.dart';
+export 'response_models/ezFileCreateResponse.dart';
+export 'response_models/ezFiles.dart';
+export 'response_models/ezProject.dart';
+export 'response_models/ezWasSuccessResponse.dart';
+export 'response_models/ezLabel/ezLabel.dart';
+export 'response_models/ezLabel/ezLabelAdditionalProps.dart';
+export 'response_models/ezLabel/ezLabelAnnotations.dart';
+export 'response_models/ezLabel/ezLabelAssociateWith.dart';
+export 'response_models/ezLabel/ezLabelData.dart';
+export 'response_models/ezLabel/ezLabelFields.dart';
+export 'response_models/user.dart';
+export 'response_models/secure_cookie.dart';
+export 'response_models/session.dart';
 
 final getIt = GetIt.instance;
 
@@ -17,16 +37,11 @@ final getIt = GetIt.instance;
 )
 void configureDependencies() => $initGetIt(getIt);
 
-void main() {
-  configureDependencies();
-  print('YOU NEED TO PROVIDE CREDENTIALS IN SOURCE CODE');
-
-  getIt<SessionEndpoints>().createSessionGet().then((value) {
-    var formData = FormData.fromMap({'email': '', 'password': ''});
-    getIt<AuthenticationEndpoints>().emailPasswordPost(formData).then((value) {
-      getIt<EzAssetEndpoints>()
-          .getGet('fe9ec808-0007-40ac-9ada-0000000008c3')
-          .then((value) => print(value.body.results[0].files?.length));
-    });
-  });
+class DevEzaApi {
+  static final AuthenticationEndpoints authenticationEndpoints = getIt<AuthenticationEndpoints>();
+  static final EzAssetEndpoints ezAssetEndpoints = getIt<EzAssetEndpoints>();
+  static final EzFileEndpoints ezFileEndpoints = getIt<EzFileEndpoints>();
+  static final EzLabelEndpoints ezLabelEndpoints = getIt<EzLabelEndpoints>();
+  static final EzProjectEndpoints ezProjectEndpoints = getIt<EzProjectEndpoints>();
+  static final SessionEndpoints sessionEndpoints = getIt<SessionEndpoints>();
 }
